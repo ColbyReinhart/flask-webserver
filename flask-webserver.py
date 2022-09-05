0c71ccb4a48f3903fd557e7997d370f71b8b659d
Colby Reinhart <colbyreinhart@gmail.com>
2022-09-05 14:54:28 -0400

    Added rest of website

--- flask-webserver.py
+++ flask-webserver.py
@@ -7,13 +7,30 @@ import os
 
 app = Flask(__name__)
 
+# Homepage
 @app.route("/")
 def homepage():
 	return render_template("homepage.html")
 
+# Favicon
 @app.route("/favicon.ico")
 def favicon():
 	return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
+
+# About me
+@app.route("/aboutme")
+def aboutme():
+	return render_template("aboutme.html")
+
+# Projects
+@app.route("/projects")
+def projects():
+	return render_template("projects.html")
+
+# Experience
+@app.route("/experience")
+def experience():
+	return render_template("experience.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
